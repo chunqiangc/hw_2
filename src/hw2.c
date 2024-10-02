7321bbefdc6e1af02375b7768c95120d3b4f9e05
chunqiangc <chunqiang.chen@stonybrook.edu>
2024-10-02 20:09:13 +0000

    print

--- src/hw2.c
+++ src/hw2.c
@@ -7,6 +7,42 @@
 
 void print_packet(unsigned int packet[])
 {
+    unsigned int int0 = packet[0];
+    unsigned int int1 = packet[1];
+    unsigned int int2 = packet[2];
+
+
+    unsigned int packet_type = (int0 >> 24) & 0xFF;
+    unsigned int address = (int2);
+    unsigned int length = int0 & 0x2FF;
+    unsigned int requester_id = (int1 >> 16) & 0xFFFF;
+    unsigned int tag = (int1 >> 8) & 0xFF;
+    unsigned int last_BE = (int1 >> 4) & 0x0F;
+    unsigned int first_BE = int1 & 0x0F;
+
+    if (packet_type == 0100)
+        printf("Packet Type : Write\n");
+    else if (packet_type == 00)
+        printf("Packet Type : Read\n");
+    else if (packet_type == 01001010)
+        printf("Packet Type : Completion\n");
+    // else printf("error\n");
+
+    printf("Address: %u\n", address);
+    printf("Length: %u\n", length);
+    printf("Requester ID: %u\n", requester_id);
+    printf("Tag: %u\n", tag);
+    printf("Last BE: %u\n", last_BE);
+    printf("1st BE: %u\n", first_BE);
+
+    if (packet_type == 0100) {
+        printf("Data: ");
+        for (unsigned int i = 0; i < length; i++) {
+            unsigned int data = packet[3 + i];
+            printf("%d  ", (int)data);
+        }
+    }
+
     (void) packet;
 }
 
--- src/hw2_main.c
+++ src/hw2_main.c
@@ -5,5 +5,15 @@
 
 int main() {
 	//Write your code here
+    unsigned int packet[] = {
+    0x40000003,
+    0x00000C75,
+    0x000000CC,
+    0x00845FED,
+    0xFFFF668F,
+    0x05888192
+    };
+
+    print_packet(packet);
     return 0;
 }
