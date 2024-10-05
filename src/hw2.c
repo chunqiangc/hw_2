147c0e22feae9e4d3254dd661d11e31f7417be7b
chunqiangc <chunqiang.chen@stonybrook.edu>
2024-10-05 20:38:03 +0000

    segmentationFault

--- src/hw2.c
+++ src/hw2.c
@@ -63,27 +63,8 @@ void store_values(unsigned int packets[], char *memory)
 
         unsigned int address = (int2);
         unsigned int length = int0 & 0x2FF;
-        // unsigned int requester_id = (int1 >> 16) & 0xFFFF;
-        // unsigned int tag = (int1 >> 8) & 0xFF;
         unsigned int last_BE = (int1 >> 4) & 0x0F;
         unsigned int first_BE = int1 & 0x0F;
-
-        // printf("Address: %u\n", address);
-        // printf("Length: %u\n", length);
-        // printf("Requester ID: %u\n", requester_id);
-        // printf("Tag: %u\n", tag);
-        // printf("Last BE: %u\n", last_BE);
-        // printf("1st BE: %u\n", first_BE);
-        // printf("Data: ");
-
-        // if (packet_type == 0100) {
-        //     for (unsigned int j = 0; j < length; j++) {
-        //         unsigned int data = packets[i + 3 + j];
-        //         printf("%d ", (int)data);
-        //     }
-        // }
-
-        // printf("\n");
 
         if (packet_type != 0100)
             break;
@@ -132,8 +113,58 @@ void store_values(unsigned int packets[], char *memory)
 
 unsigned int* create_completion(unsigned int packets[], const char *memory)
 {
+    unsigned int* completions = malloc(1024 * sizeof(unsigned int));
     
+    int index = 0;
+    for (unsigned int i = 0; ; ) {
+
+        unsigned int int0 = packets[0];
+        unsigned int int1 = packets[1];
+        unsigned int int2 = packets[2];
+
+
+        unsigned int packet_type = (int0 >> 24) & 0xFF;
+        if (packet_type != 00)
+            break;
+
+        unsigned int address = (int2);
+        unsigned int length = int0 & 0x2FF;
+        unsigned int requester_id = (int1 >> 16) & 0xFFFF;
+        unsigned int tag = (int1 >> 8) & 0xFF;
+        unsigned int lower_address = address & 0x7F;
+
+        unsigned int beforeBound = 0;
+        for (unsigned int j = address; j < address + length * 4; j++) {
+            if (j == 0x4000) {
+                beforeBound = (j - address) / 4;
+                break;
+            }
+            beforeBound = length;
+        }
+
+        completions[index++] = (0x094000 << 10) | length;
+        completions[index++] = (((0x00DC << 4) | 0x0) << 10) | length * 4;
+        completions[index++] = ((((requester_id << 8) | tag) << 8) | lower_address);
+
+        for (unsigned int j = 0; j < beforeBound; j++) {
+            completions[index++] = (((((memory[address + j * 4 + 3] << 8) | memory[address + j * 4 + 2]) << 8) | memory[address + j * 4 + 1]) << 8) | memory[address + j * 4];
+
+        }
+        
+        if (beforeBound != length) {
+            completions[index++] = (0x094000 << 10) | (length - beforeBound);
+            completions[index++] = (((0x00DC << 4) | 0x0) << 10) | (length - beforeBound) * 4;
+            completions[index++] = ((((requester_id << 8) | tag) << 8) | (0x4000 & 0x7F));
+
+            for (unsigned int j = 0; j < length - beforeBound; j++) {
+            completions[index++] = (((((memory[address + j * 4 + 3] << 8) | memory[address + j * 4 + 2]) << 8) | memory[address + j * 4 + 1]) << 8) | memory[address + j * 4];
+
+            }
+        }
+
+        i += length + 3;
+    }
     (void)packets;
     (void)memory;
-	return NULL;
+	return completions;
 }
